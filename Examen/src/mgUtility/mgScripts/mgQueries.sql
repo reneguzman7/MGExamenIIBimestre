56a90e360047bd9dfd9074e3c365f8e487b63e0e
reneguzman7 <121918879+reneguzman7@users.noreply.github.com>
2023-09-17 16:52:42 -0500

    Intento Join

--- Examen/src/mgUtility/mgScripts/mgQueries.sql
+++ Examen/src/mgUtility/mgScripts/mgQueries.sql
@@ -78,8 +78,7 @@ INSERT INTO TablaJoin (Usuario, Tipo_Coordenada, Coordenada, Arsenal, Dia, Hora)
 ('rene.guzman@epn.edu.ec', 'North', '01', 'Avion','Lunes', '01-02');
 
 
-
---Queries de usuarios con claves encriptadas con MD5
+-- Queries de usuarios con claves encriptadas con MD5
 INSERT INTO MG_USUARIOS (Usuario, Contrasenia) VALUES ("profe", "827ccb0eea8a706c4c34a16891f84e7b");
 INSERT INTO MG_USUARIOS (Usuario, Contrasenia) VALUES ("rene.guzman@epn.edu.ec", "502aa294fa760f42958ed49fd06c1d07");
 INSERT INTO MG_USUARIOS (Usuario, Contrasenia) VALUES ("harryson.montesdeoca@epn.edu.ec", "225869c60b61a8d5bf83fb61bafb5ce4");
